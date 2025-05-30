6aa01eec2230a73fa705d2a8ff463699f21b1daf
永井聖士 <daizuomame0903@gmail.com>
2025-05-30 16:42:17 +0900

    calc.rbで計算の練習

--- calc.rb
+++ calc.rb
@@ -19,3 +19,48 @@ def square(num)
     return num * num
 end
 puts square(5)
+
+def cashier(price1, price2)
+    puts price1 + price2
+end
+cashier(100, 200)
+
+def cashier(price1, price2)
+    puts (price1 + price2) * 1.1
+end
+cashier(100, 200)
+
+def cashier(price1, price2)
+    puts ((price1 + price2) * 1.1).floor
+end
+cashier(100, 200)
+
+def cashier(prices)
+    p prices
+end
+cashier([100, 200, 300])
+
+def cashier(prices)
+    prices.each do |price|
+    puts price
+    end
+end
+cashier([100, 200, 300])
+
+def cashier(prices)
+    sum = 0
+    prices.each do |price|
+    sum += price
+    end
+    puts sum
+end
+cashier([100, 200, 300])
+
+def cashier(prices)
+    sum = 0
+    prices.each do |price|
+    sum += price
+    end
+    puts "#{(sum * 1.1).floor}"
+end
+cashier([100, 200, 300])
