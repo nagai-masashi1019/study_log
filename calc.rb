f608acf91b76b605e68981b263898921972f58ed
永井聖士 <daizuomame0903@gmail.com>
2025-05-30 14:42:22 +0900

    初回のコミット

---
+++ calc.rb
@@ -0,0 +1,21 @@
+puts "Hello World!"
+
+def greet(message)
+    puts message
+end
+greet("Hello World!")
+
+def add(a, b)
+    return a + b
+end
+puts add(3, 4)
+
+def multiply(x, y)
+    return x * y
+end
+puts multiply(2, 3)
+
+def square(num)
+    return num * num
+end
+puts square(5)
