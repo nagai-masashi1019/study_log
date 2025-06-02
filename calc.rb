20425c0ee1f78aa2ec0c0d108eb6bb8d396a9c94
永井聖士 <daizuomame0903@gmail.com>
2025-06-02 14:32:29 +0900

    ハッシュの勉強

--- calc.rb
+++ calc.rb
@@ -64,3 +64,56 @@ def cashier(prices)
     puts "#{(sum * 1.1).floor}"
 end
 cashier([100, 200, 300])
+
+hash = { "name" => "Alice", "age" => 30 }
+puts hash ["name"]
+puts hash ["age"]
+
+hash = {"name" => "Alice" }
+hash["age"] = 30
+hash["name"] = "Bob"
+puts hash
+
+hash = { name: "Alice", age: 30 }
+puts hash [:name]
+puts hash [:age]
+
+hash = { name: "Alice", age: 30 }
+hash.each do |key, value|
+    puts "#{key}: #{value}"
+end
+
+def cashier(items)
+p items
+end
+items = [{ name: "apple", price: 100 }, { name: "banana", price: 200 }, { name: "orange", price: 300 }]
+cashier(items)
+
+def cashier(items)
+    items.each do |item|
+    p item
+    end
+end
+items = [{ name: "apple", price: 100 }, { name: "banana", price: 200 }, { name: "orange", price: 300 }]
+cashier(items)
+
+def cashier(items)
+    sum = 0
+    items.each do |item|
+    puts "商品名: #{item[:name]}"
+    puts "価格: #{item[:price]}"
+    end
+end
+items = [{ name: "apple", price: 100 }, { name: "banana", price: 200 }, { name: "orange", price: 300 }]
+cashier(items)
+
+def cashier(items)
+    sum = 0
+    items.each do |item|
+        puts "#{item[:name]}が1点"
+        sum += item[:price]
+    end
+    puts "合計金額は#{(sum * 1.1).floor}"
+end
+items = [{ name: "apple", price: 100 }, { name: "banana", price: 200 }, { name: "orange", price: 300 }]
+cashier(items)
